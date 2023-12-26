58d8bd9631a71e106a9c016fd9a4d7829043fa34
Aragami1408 <vucaominh1408@gmail.com>
2023-12-26 23:34:02 +0700

    Change include dir for clangd and adding libpath for vulkan

--- SKXEngine/build.sh
+++ SKXEngine/build.sh
@@ -13,8 +13,8 @@ assembly="SKXEngine"
 compilerFlags="-g -shared -fdeclspec -fPIC -std=c++17"
 # -fms-extensions 
 # -Wall -Werror
-includeFlags="-Isrc -I$VULKAN_SDK/include"
-linkerFlags="-lvulkan -lxcb -lX11 -lX11-xcb -lxkbcommon -L$VULKAN_SDK/lib -L/usr/X11R6/lib"
+includeFlags="-Isrc -I$VULKAN_SDK/macOS/include -I$VULKAN_SDK/include"
+linkerFlags="-lvulkan -lxcb -lX11 -lX11-xcb -lxkbcommon -L$VULKAN_SDK/lib -L$VULKAN_SDK/macOS/lib -L/usr/X11R6/lib"
 defines="-D_DEBUG -DSKX_EXPORT"
 
 echo "Building $assembly..."
